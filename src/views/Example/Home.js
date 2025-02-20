15339a3865546acad22a77296bb24de830dd3aec
Vuong Dac Bac <bac27022002@gmail.com>
2025-02-21 00:04:50 +0700

    video 24

--- src/views/Example/Home.js
+++ src/views/Example/Home.js
@@ -1,9 +1,17 @@
 import React from "react";
-
+import { withRouter } from "react-router";
 class Home extends React.Component {
+  componentDidMount() {
+    setTimeout(() => {
+      this.props.history.push("/todo");
+    }, 3000);
+  }
+
+  //HOC: higher order component
   render() {
+    console.log(">>> check props: ", this.props);
     return <div>Hello world from Homepage with dacbac2702</div>;
   }
 }
 
-export default Home;
+export default withRouter(Home);
