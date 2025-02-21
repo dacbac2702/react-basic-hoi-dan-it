716bd1241d96daa8732f9314e3086ae25c56bf01
Vuong Dac Bac <bac27022002@gmail.com>
2025-02-22 00:14:44 +0700

    video 32

---
+++ src/store/reducers/rootReducer.js
@@ -0,0 +1,12 @@
+const initState = {
+  users: [
+    { id: 1, name: "BPR" },
+    { id: 2, name: "dacbac2702" },
+  ],
+};
+
+const rootReducer = (state = initState, action) => {
+  return state;
+};
+
+export default rootReducer;
