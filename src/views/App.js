7ccbc8771fc6af0115106213ef7f6e90e61f3cfd
Vuong Dac Bac <bac27022002@gmail.com>
2025-02-21 15:25:20 +0700

    video 27

--- src/views/App.js
+++ src/views/App.js
@@ -8,6 +8,7 @@ import ListUser from "./Users/ListUser";
 import Nav from "./Nav/Nav";
 import Home from "./Example/Home";
 import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+import DetailUser from "./Users/DetailUser";
 
 /**
  * 2 components: class component / function component ( function, arrow)
@@ -31,8 +32,11 @@ function App() {
             <Route path="/about">
               <MyComponent />
             </Route>
-            <Route path="/user">
+            <Route path="/user" exact>
               <ListUser />
+            </Route>
+            <Route path="/user/:id">
+              <DetailUser />
             </Route>
           </Switch>
         </header>
